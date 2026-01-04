417a86fb1607978284a35e82d3151fe0d7262b75
Roger Rodriguez Texido <rogerkael@gmail.com>
2026-01-04 09:04:25 -0800

    Initial commit

---
+++ binary-mobile.scm
@@ -0,0 +1,69 @@
+(define (make-mobile left right)
+	(list left right)
+)
+
+(define (left-branch mobile) (car mobile))
+(define (right-branch mobile) (car (cdr mobile)))
+
+(define (make-branch length structure)
+	(list length structure)
+)
+
+(define (branch-length b) (car b))
+(define (branch-structure b) (car (cdr b)))
+
+(define (branch-total-weight b)
+	(let ((current-structure (branch-structure b)))
+		(if (pair? current-structure)
+		(total-weight current-structure)
+		current-structure
+		)
+	)
+)
+
+(define (total-weight m)
+	(+ (branch-total-weight (left-branch m)) (branch-total-weight (right-branch m)))
+)
+
+(define (torque b) (* (branch-length b) (branch-total-weight b)))
+
+(define (is-balanced m)
+	(if (pair? m) 
+		(and
+			(= (torque (left-branch m)) (torque (right-branch m)))
+			(is-balanced (branch-structure (left-branch m)))
+			(is-balanced (branch-structure (right-branch m)))
+		)
+		#t
+	)
+)
+;(define my-mobile (make-mobile (make-branch 2 (make-mobile (make-branch 4 6)) (make-branch 5
+
+(define six (make-branch 6 8))
+(define seven (make-branch 7 11))
+(define eight (make-branch 8 10))
+(define nine (make-branch 9 12))
+(define four (make-branch 4 6))
+(define five (make-branch 5 (make-mobile six seven)))
+(define two (make-branch 2 (make-mobile four five)))
+(define three (make-branch 3 (make-mobile eight nine)))
+(define mobile-one (make-mobile two three))
+
+(define balanced-mobile-one (make-mobile (make-branch 4 5) (make-branch 5 4)))
+(define balanced-mobile-two (make-mobile (make-branch 4 5) (make-branch 5
+				(make-mobile (make-branch 2 2) (make-branch 2 2)))))
+
+
+(display mobile-one)
+(total-weight mobile-one)
+
+(is-balanced mobile-one)
+
+(display balanced-mobile-one)
+(is-balanced balanced-mobile-one)
+
+(display balanced-mobile-two)
+(is-balanced balanced-mobile-two)
+(is-balanced (branch-structure (right-branch balanced-mobile-two)))
+(total-weight (branch-structure (right-branch balanced-mobile-two)))
+
---
+++ lists.scm
@@ -0,0 +1,104 @@
+(define (list-get l i)
+	(define (list-get-impl l i c)
+		(if (= c i)
+			(car l)
+			(list-get-impl (cdr l) i (+ c 1))
+		)
+	)
+	(list-get-impl l i 0)
+)
+
+
+
+
+(define myl (list 4 5 6))
+(list-get myl 0)
+(list-get myl 1)
+(list-get myl 2)
+
+(cons 1 (list 2 3))
+(cons (list 2 3) 1)
+
+(define (reverse-list l)
+	;(define (iter left right)
+	;	(if (null? left)
+	;		right
+	;		(iter (cdr left) (cons (car left) right))
+	;	)
+	;)
+	;(iter l '())
+	(if (= 1 (length l))
+		l
+		(append (reverse-list (cdr l)) (list (car l)))
+	)
+)
+
+(display (reverse-list (list 1 2 3 4)))
+
+(define (same-parity . els)
+	(define (iter first remaining result)
+		(if (null? remaining)
+			result
+			(let ((current (car remaining)))
+				(if (= (remainder first 2) (remainder current 2))
+					;(iter first (cdr remaining) (cons current result))
+					(iter first (cdr remaining) (append result (list current)))
+					(iter first (cdr remaining) result)
+				)
+			)
+		)
+	)
+	(iter (car els) els '())
+)
+
+(same-parity 1 2 3 4 5)
+(same-parity 2 3 4 5 6)
+
+(define (for-each proc l)
+	(if (null?  l)
+		#f
+		(let ((c (car l)))
+			(proc c)
+			(for-each proc (cdr l))
+		)
+	)
+)
+
+(for-each (lambda (x) (newline) (display x)) (list 123 4325 34324))
+
+(define (uniques l)
+	(define (unique-impl l r last)
+		;(newline )
+		;(display l)
+		;(newline )
+		;(display r)
+		;(newline )
+		(cond
+			((= (length l) 0) r)
+			(else (if (= (car r) (car l))
+				(unique-impl (cdr l) r)
+				(unique-impl (cdr l) (cons (car l) r))
+				)
+			)
+		)
+	)
+	(unique-impl (cdr l) (list (car l)))
+)
+
+(display "uniques:")
+(newline )
+(uniques (list 1 2 2 3 3 3))
+
+
+(define (fringe l)
+	(if (pair? l)
+		(if (null? (cdr l))
+			(fringe (car l))
+			(append (fringe (car l)) (fringe (cdr l)))
+		)
+		(list l)
+	)
+)
+
+(define tree (list (list 1 2) (list (list 4 5) ) 5 (list 3 4)))
+(fringe tree)
