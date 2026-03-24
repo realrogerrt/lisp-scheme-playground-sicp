ad62f4d21067eb9960a3bdc123178ed9a6bfd7b7
Roger Rodriguez Texido <rrtexido@amazon.com>
2026-03-24 09:56:21 -0700

    Vectors and frames operations

--- painters.scm
+++ painters.scm
@@ -1,9 +1,11 @@
 ;2.44
 (define (beside a b) 
   (string "(beside " a " " b ")")
+  ;(lambda () (beside a b))
 )
 (define (below a b)
   (string "(below " a " " b ")")
+  ;(lambda () (below a b))
 )
 
 ;2.45
@@ -40,4 +42,40 @@
 	(beside (below painter top-left)
 		(below bottom-right corner))))))
 
-(corner-split 1 3)
+;(corner-split 1 3)
+;(beside 1 2)
+
+;;2.46
+
+;make-vect xcor-vect and ycor-vect
+;add-vect, sub-vect, and scale-vect
+
+(define (make-vect a b) (cons a b))
+(define (xcor-vect v) (car v))
+(define (ycor-vect v) (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+	     (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+	     (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v)) (* s (ycor-vect v))))
+
+(define (make-frame o e1 e2) (list o e1 e2))
+(define (origin-frame f) (car f))
+(define (edge1-frame f) (car (cdr f)))
+(define (edge2-frame f) (car (cdr (cdr f))))
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+      (origin-frame frame)
+      (add-vect (scale-vect (xcor-vect v) (edge1-frame frame))
+		(scale-vect (ycor-vect v) (edge2-frame frame))))))
+
+(define a-frame (make-frame (make-vect 1 2) (make-vect 3 4) (make-vect 5 6)))
+((frame-coord-map a-frame) (make-vect 0 0))
